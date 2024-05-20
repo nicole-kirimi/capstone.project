06c393c0cfe42a55e77db9bb4e35fd067f330116
nicole <nicolekirimi123@gmail.com>
2024-05-20 12:15:10 +0300

    add index.js

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
 
+const http = require('http');
+const url = require('url');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const users = [];
+
+const JWT_SECRET = 'your_jwt_secret';
+
