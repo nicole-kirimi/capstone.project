257197559b62dc6c84e3cf4aef2267016634c2a1
nicole <nicolekirimi123@gmail.com>
2024-05-20 11:30:48 +0300

    add app.js and add function

---
+++ src/app.js
@@ -0,0 +1,6 @@
+async function handleLogin(event) {
+    event.preventDefault();
+
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-/** This is the entry point of your app */
+
+
