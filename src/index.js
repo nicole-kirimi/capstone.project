853c010833b24333d02e142503db7bd698ad3e4f
nicole <nicolekirimi123@gmail.com>
2024-05-20 15:55:36 +0300

    remove import css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+
 
 document.getElementById('login-form')?.addEventListener('submit', async function(event) {
     event.preventDefault();
